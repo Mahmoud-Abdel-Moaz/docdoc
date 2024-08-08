b3355a009033101f2dd1c1487c10e5b15113f5b4
Mahmoud Abdel Moaz <mabdelmoez@nxt.so>
2024-08-08 22:11:00 +0300

    - Add Font Weight helper

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:docdoc/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,11 +8,13 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login Screen"),
-      ),
-      body: const Center(
-        child: Text("Login Screen is working"),
+      body:  SafeArea(
+        child: Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 30.w,vertical: 30.h),
+          child: ListView(
+            children: [Text("Welcome Back",style: TextStyles.font24BlueBold,)],
+          ),
+        ),
       ),
     );
   }
